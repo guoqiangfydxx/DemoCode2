802ceea82052ebf2af5b3704850e3a0c1fccae01
guoqiang <18852852576@163.com>
2021-07-20 22:04:57 +0800

    feat: ass scss

--- src/pages/scss/index.tsx
+++ src/pages/scss/index.tsx
@@ -5,8 +5,9 @@ import './index.scss';
 function Demo() {
   return (
     <div className="container">
-      <p className="p1">这是一个p元素</p>
+      <p className="p1 foo">这是一个p元素</p>
       <h2>这是一个h2元素</h2>
+      <a href="https://www.baidu.com">百度</a>
     </div>
   );
 }
