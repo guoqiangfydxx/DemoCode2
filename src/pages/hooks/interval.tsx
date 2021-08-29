dd8ccef1238a534ce910fc82d31d42dccdbfa587
guoqiang <qiang2.guo@ximalaya.com>
2021-08-29 18:22:15 +0800

    feat: add 定时器

--- src/pages/hooks/interval.tsx
+++ src/pages/hooks/interval.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Button } from 'antd';
 function TestInterval() {
   const [downloadPrecent, setDownLoadPrecent] = useState<Map<string, number>>(
@@ -12,6 +12,7 @@ function TestInterval() {
   const [isStartDownLoad, setIsStartDownLoad] = useState<Map<string, boolean>>(
     new Map()
   );
+  const [num, setNum] = useState(0);
   const count = useRef<number>(1);
 
   function handleClick() {
@@ -26,6 +27,26 @@ function TestInterval() {
     }, 3000);
   }
 
+  useEffect(() => {
+    console.log('mount');
+    setInterval(() => {
+      downloadPrecent.set(`${count.current}`, count.current + 1);
+      downLoadStatus.set(`${count.current}`, count.current + 2);
+      isStartDownLoad.set(`${count.current}`, true);
+      setDownLoadPrecent(new Map(downloadPrecent));
+      setDownLoadStatus(new Map(downLoadStatus));
+      setIsStartDownLoad(new Map(isStartDownLoad));
+      count.current++;
+    }, 5000);
+  }, []);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setNum((num) => num + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
   console.log('downloadPrecent', downloadPrecent);
   console.log('downLoadStatus', downLoadStatus);
   console.log('isStartDownLoad', isStartDownLoad);
@@ -33,6 +54,7 @@ function TestInterval() {
     <div>
       <span>定时器测试</span>
       <Button onClick={handleClick}>测试</Button>
+      <p>num的值为：{num}</p>
     </div>
   );
 }
